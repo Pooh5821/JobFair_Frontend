807d388eb0cc58aaf82a063697f55c0333578731
Pooh5821 <pooh5821ingkanunt@gmail.com>
2025-04-18 23:10:46 +0700

    add auth

---
+++ src/features/authService.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+const API_URL='http://localhost:5000/api/v1/auth/'
+
+const login = async (userData) => {
+    try {
+        const response = await axios.post(API_URL+'login',userData)
+        if(response.data) {
+            localStorage.setItem('user',response.data.name)
+        }
+        return response.data.name
+    } catch(error) {
+        console.log('authService: login')
+        console.log(error);
+    }
+}
+const logout = () => {
+    localStorage.setItem('user',null)
+}
+const authService ={
+    login, logout
+}
+
+export default authService;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,76 @@
+import { useState, useEffect } from "react";
+import { toast } from "react-toastify";
+import { FaSignInAlt } from "react-icons/fa";
+import { useSelector, useDispatch } from 'react-redux'
+import { login,reset } from '../features/auth/authSlice'
+import { useNavigate } from 'react-router-dom'
+
+function Login() {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const {email, password} = formData;
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const {user, isLoading, isError, isSuccess, message} = useSelector((state)=>state.auth)
+
+    useEffect(()=>{
+        if(isError){
+            toast.error(message)
+        }
+        if(isSuccess){
+            navigate('/')
+        }
+        dispatch(reset())
+    }, [isError, isSuccess, user, message, navigate, dispatch])
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }));
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        const data = {
+            email,
+            password
+        }
+
+        dispatch(login(data))
+    }
+
+    return (
+        <>
+            <section className='heading'>
+                <h1>
+                    <FaSignInAlt />Login
+                </h1>
+                <p>Please login to view bookings</p>
+            </section>
+
+            <section className='form'>
+                <form onSubmit={onSubmit}>
+                    <div className='form-group'>
+                        <input type='email' name='email' value={email} onChange={onChange} placeholder="Enter your email" required />
+                    </div>
+
+                    <div className='form-group'>
+                        <input type='password' name='password' value={password} onChange={onChange} placeholder="Enter your password" required />
+                    </div>
+
+                    <div className='form-group'>
+                        <button className='btn btn-block'>Submit</button>
+                    </div>
+                </form>
+            </section>
+        </>
+    );
+}
+
+export default Login;
