7b60f875a5ae4c3d8b98daaf4593541c3a1f653c
Pooh5821 <pooh5821ingkanunt@gmail.com>
2025-04-19 17:31:53 +0700

    update booking dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { toast } from "react-toastify";
 import { useSelector, useDispatch } from 'react-redux'
-import { reset, getBookings } from "../features/bookingSlice";
+import { reset, getBookings } from "../features/booking/bookingSlice";
 import { useNavigate } from 'react-router-dom'
 import Booking from "../components/Booking";
 
